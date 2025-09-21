43f1039a9dfc9519c0456db43fcb78816399baff
Ethan <ethanmurray180@gmail.com>
2025-09-21 20:16:23 +0100

    set up checkout.py

---
+++ checkout.py
@@ -0,0 +1,9 @@
+from board import board
+import sys
+
+def get_checkout(score, throws=3, darts=[]):
+    pass
+
+if __name__ == "__main__":
+    score = int(sys.argv[1])
+    print(get_checkout(score))
