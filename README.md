cad89af56617dbb8e66110be18f57afbfedfb3ea
Ethan <ethanmurray180@gmail.com>
2025-09-22 17:18:02 +0100

    updated readme with improvements

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dart-checkout
 determine a winning throw of 501
+
+The current solution is extremely naive and could benefit from improvements.
+One improvement is that the solution given might not be the most "favourable"
+    - e.g. a more favourable throw would be D5 to checkout 10, instead this solution gives 8 and D1
+
+
--- checkout.py
+++ checkout.py
@@ -2,21 +2,26 @@ from board import board
 import sys
 
 def get_checkout(score, throws=3, darts=[]):
+    #Base cases where a winning throw cannot be made
     if (score < 2 or score > 170) or (throws == 0 and score != 0):
         return []
     
     for location, value in board.items():
+        #Not a possible throw since the value on the board is too high
         if value > score:
             continue
 
         darts.append(location)
-        if value == score and location[0] == "D":
+        #If the last dart is a double AND there is no remaining score to be made, this is one possible winning throw
+        if value == score and location[0] == "D": 
             return darts
         
+        #Recursively search for the next possible throw
         res = get_checkout(score - value, throws - 1, darts)
         if res:
             return res
         
+        #Backtrack if not a valid solution
         darts.pop()
 
 if __name__ == "__main__":
